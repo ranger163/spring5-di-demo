8b0ed2a87b8d5aa20f85490ce1d075f62aac62bb
ranger163 <ahmed.nassar163@gmail.com>
2020-05-15 13:51:53 +0200

    Creating MyController.java Learning about spring boot framework context

--- src/main/java/me/inassar/didemo/DiDemoApplication.java
+++ src/main/java/me/inassar/didemo/DiDemoApplication.java
@@ -1,13 +1,17 @@
 package me.inassar.didemo;
 
+import me.inassar.didemo.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DiDemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DiDemoApplication.class, args);
+        ApplicationContext context = SpringApplication.run(DiDemoApplication.class, args);
+        MyController controller = (MyController) context.getBean("myController");
+        controller.hello();
     }
 
 }
---
+++ src/main/java/me/inassar/didemo/controllers/MyController.java
@@ -0,0 +1,12 @@
+package me.inassar.didemo.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    public String hello() {
+        System.out.println("Hello!!");
+        return "foo";
+    }
+}
